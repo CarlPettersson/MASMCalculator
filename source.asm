93dfb4b4c00a799100d0d419e2352997667c8bdb
CarlPettersson <54657589+CarlPettersson@users.noreply.github.com>
2022-05-31 16:00:20 +0200

    comments and hex
    commented the code and changed decimal to hexadecimal where conversion between encoding and signs is concerned

--- source.asm
+++ source.asm
@@ -4,11 +4,11 @@
  include Irvine32.inc 
 
  .data
-	MAX = 80
+	MAX = 50				
 	stringIn BYTE MAX+1 DUP (?)
-	stringSize DWORD ?
-	newline BYTE 13, 10, 0
-	divBy0 BYTE "NaN", 13, 10, 0
+	stringSize DWORD ?		
+	newline BYTE 13, 10, 0	
+	divBy0 BYTE "NaN", 13, 10, 0			; data variables
 	first BYTE ?
 	num DWORD 0
 	result DWORD 0
@@ -18,11 +18,11 @@
  .code
  main PROC
  start:
-	xor esi, esi
+	xor esi, esia
 	mov eax, 0
 	mov num, eax
 	mov result, 0
-	mov first, 1
+	mov first, 1				; reset variables and read input
 	mov edx, offset stringIn
 	mov ecx, MAX
 	call readstring
@@ -32,119 +32,121 @@
 	mov ebx, 10
 	L1:
 		movzx eax, stringIn[esi]
-		cmp eax, 47
+		cmp eax, 71h				; if input is q quit
+		jz quit						  
+		cmp eax, 30h				; check if number
 		jbe @f
-		cmp eax, 58
-		jae @f
-			sub eax, 48	
-			mov tempnum, eax
-			mov eax, num
+		cmp eax, 39h				
+		ja @f
+			sub eax, 30h			; convert to real number from utf-8 encoding
+			mov tempnum, eax		 
+			mov eax, num			
 			mul ebx
 			add eax, tempnum
 			mov num, eax
 		@@:
-		cmp eax, 42
+		cmp eax, 2ah				
 		je @f
-		cmp eax, 43
+		cmp eax, 2bh				
 		je @f
-		cmp eax, 45
+		cmp eax, 2dh				; check if correct symbol
 		je @f
-		cmp eax, 47
+		cmp eax, 2fh				
 		je @f
-		cmp eax, 94
-		jne endif1
+		cmp eax, 5eh				
+		jne endif1			
 		@@:
-			push eax
-			jmp GoHere
+			push eax				
+			jmp GoHere				; if correct symbol start to calculate
 		endif1:
 		GoBack:
-		add esi, 1
+		add esi, 1					
 		add ecx, 1
-	cmp ecx, stringSize
+	cmp ecx, stringSize				; loop through the whole input string
 	jle L1
 GoHere:
 	mov eax, result
-	cmp whatsign, 42		
+	cmp whatsign, 2ah				
 	jne @f
-	cmp first, 1
+	cmp first, 1					
 	je else1
-	mul num
+	mul num							; if not first sign multiply number
 	jmp endif2
 	else1:
-		mov eax, num
+		mov eax, num				
 		jmp endif2
 	@@:
-	cmp whatsign, 43
+	cmp whatsign, 2bh				
 	jne @f
-		add eax, num
+		add eax, num				; add number
 		jmp endif2
 	@@:
-	cmp whatsign, 47
+	cmp whatsign, 2fh				
 	jne @f
-		cmp num, 0
+		cmp num, 0					; if div by zero give error and go to beginning
 		jne divelse1
 			mov edx, offset divBy0
 			call writestring
 			jmp start
 		divelse1:
-			cmp first, 1
+			cmp first, 1			; if first sign jmp firstdiv
 			je firstDiv
-				div num
-				cmp edx, 0
+				div num				; div by num
+				cmp edx, 0			; check if rest is 0
 				je endif2
-					test eax, eax
+					test eax, eax	; check if negative
 					jl neg2
-					call writedec
+					call writedec	
 					jmp pos2
 					neg2:
-					call writeint
+					call writeint	; write num before decimal place
 					pos2:
 					mov ebx, num
-					mov al, '.'
-					call writechar
-					mov ecx, 3
+					mov al, '.'		
+					call writechar	; write decimal 
+					mov ecx, 3		; number of deciaml places
 					L3:
-						imul eax, edx, 10
-						xor edx, edx
-						div ebx
-						call writeDec
+						imul eax, edx, 10	
+						xor edx, edx		
+						div ebx				; write decimal places
+						call writeDec		
 					Loop L3
-					mov edx, offset newline
-					call writestring
-					jmp start
+					mov edx, offset newline	
+					call writestring		; write a newline
+					jmp start				; go to beginning
 			firstDiv:
-				mov eax, num
+				mov eax, num				
 				jmp endif2
 	@@:
-	cmp whatsign, 45
+	cmp whatsign, 2dh			
 	jne @f
-	cmp first, 1
+	cmp first, 1				
 	je subelse1
-		sub eax, num
+		sub eax, num				; sub eax by num
 		jmp endif2
 	subelse1:
 		mov eax, num
 		jmp endif2
 	@@:
-	cmp whatsign, 94 
+	cmp whatsign, 5eh
 	jne endif2
 	cmp first, 1
 	jne powerelse1
 		mov eax, num
 		jmp endif2
 	powerelse1:
-		cmp num, 0
+		cmp num, 0	
 		jne	powerelif1
-			mov eax, 1
+			mov eax, 1				; if exponent is 0 answer is 1
 			jmp endif2
 		powerelif1:
 		cmp num, 1
 		jne powerelse2
-			jmp endif2
+			jmp endif2				; if exponent is 1 answer is input number
 		powerelse2:
 			push ecx
 			push esi
-			mov ecx, num
+			mov ecx, num			; else multiply exponent number of times
 			sub ecx, 1
 			mov esi, eax
 			L2:
@@ -164,37 +166,37 @@ GoHere:
 	mov num, 0
 	mov first, 0
 	pop eax
-	cmp eax, 42
+	cmp eax, 2ah
 	jne @f
-		mov whatSign, '*'
+		mov whatSign, 2ah
 		jmp endif4
 	@@:
-	cmp eax, 45
+	cmp eax, 2dh
 	jne @f
-		mov whatSign, '-'
+		mov whatSign, 2dh
+		jmp endif4
+	@@:							; set correct sign for loop
+	cmp eax, 2bh
+	jne @f
+		mov whatSign, 2bh
 		jmp endif4
 	@@:
-	cmp eax, 43
+	cmp eax, 2fh
 	jne @f
-		mov whatSign, '+'
+		mov whatsign, 2fh
 		jmp endif4
 	@@:
-	cmp eax, 47
-	jne @f
-		mov whatsign, '/'
-		jmp endif4
-	@@:
-	cmp eax, 94
+	cmp eax, 5eh
 	jne endif4
-		mov whatSign, '^'
+		mov whatSign, 5eh
 	endif4:
 	mov eax, result
 	cmp ecx, stringSize
-	jle GoBack
+	jle GoBack					; if whole input is not done go back
 	test eax, eax
 	jl neg1
 	call writeDec
-	jmp pos1
+	jmp pos1					; write out result
 	neg1:
 	call writeint
 	pos1:
@@ -202,6 +204,6 @@ GoHere:
 	call writestring
 	jmp start
 quit:
-  INVOKE ExitProcess, eax
+  INVOKE ExitProcess, 0			; quit program with code 0
  main ENDP
 END main
